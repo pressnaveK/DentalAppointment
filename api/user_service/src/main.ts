168333cb0d8dab33562cb05c07911d304681bb98
pressnaveK <pressnavek@gmail.com>
2025-10-05 12:11:05 +0400

    this

--- api/user_service/src/main.ts
+++ api/user_service/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
     origin: true, // Configure appropriately for production
     credentials: true,
   });
-  //jkjkjkj jkjkjk kjjkjghg jkjjkj
+  //jkjkjkj jkjkjk kjjkjghg jkjjkj mnknmnm
   
   await app.listen(process.env.PORT ?? 3000);
 }
